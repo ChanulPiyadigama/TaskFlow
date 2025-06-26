992965decc8c285bae1f3083921655b27a87e8e0
Chanul Piyadigama <chanulpiyadi@gmail.com>
2025-06-26 17:39:08 +1000

    uploading frontend build

--- timer_BACKEND/index.js
+++ timer_BACKEND/index.js
@@ -64,10 +64,6 @@ const start = async () => {
         })
     )
 
-    const PORT=4000
-    httpServer.listen(PORT, () => {
-        console.log(`🚀 Server ready at http://localhost:${PORT}`)
-    })
 
 }
 
